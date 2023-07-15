cfef8efd7f8401dd9f2c7e39507b873f43940942
Alex Ghiurau <20340987+alexghiurau@users.noreply.github.com>
2023-07-15 14:14:12 +0100

    docs page, update packages

--- src/pages/documentation/index.tsx
+++ src/pages/documentation/index.tsx
@@ -1,3 +1,24 @@
+import { Box, Grid, GridItem, Heading, VStack, Text } from '@chakra-ui/react';
+
 export default function Documentation() {
-  return <div>Documentation - work in progress</div>;
+  return (
+    <>
+      <Box h={'calc(100vh - 60px)'}>
+        <Grid h={'100%'} templateRows='1fr' templateColumns='1fr 6fr' gap={1}>
+          <GridItem bg='gray.100'>
+            <VStack p={'1rem'}>
+              <Text>Introduction</Text>
+              <Text>Installation</Text>
+              <Text>Usage</Text>
+            </VStack>
+          </GridItem>
+          <GridItem>
+            <Box p={'1rem'}>
+              <Heading>Documentation</Heading>
+            </Box>
+          </GridItem>
+        </Grid>
+      </Box>
+    </>
+  );
 }
